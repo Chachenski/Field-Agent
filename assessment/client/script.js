c0eca7afd12dccf15327bae1bef3dd51cb84244d
Chen <xchendd214@gmail.com>
2023-06-14 10:07:05 -0400

    helper methods added and removed columns agencies and aliases

--- assessment/client/script.js
+++ assessment/client/script.js
@@ -1,6 +1,6 @@
+const mainContent = document.getElementById("mainContent");
 const displayAllButton = document.getElementById("displayAllButton");
 const addAgentButton = document.getElementById("addAgentButton");
-const mainContent = document.getElementById("mainContent");
 
 // Event listeners
 displayAllButton.addEventListener("click", displayAllAgents);
@@ -35,8 +35,6 @@ function renderList(Agents) {
         <td>${agent.lastName}</td>
         <td>${agent.dob}</td>
         <td>${agent.heightInInches}</td>
-        <td>${agent.agencies}</td>
-        <td>${agent.aliases}</td>
         <td>
             <button onclick="handleEditAgent(${agent.id})">Edit</button>
             <button onclick="handleDeleteAgent(${agent.id})">Delete</button>
@@ -134,3 +132,84 @@ function handleDeleteAgent(agentId) {
       .catch(console.log);
   }
 };
+
+function handleAddAgent() {
+  setCurrentView("Form");
+}
+
+function handleEditAgent(agentId) {
+  const agent = agent.find(
+    (agent) => agent.id === agentId
+  );
+  document.getElementById("firstName").value = agent.firstName;
+  document.getElementById("middleName").value = agent.middleName;
+  document.getElementById("lastName").value = agent.lastName;
+  document.getElementById("dob").value = agent.dob;
+  document.getElementById("heightInInches").value = agent.heightInInches;
+  console.log(agent);
+
+  document.getElementById("formHeading").innerText = "Update Agent";
+  document.getElementById("formSubmitButton").innerText = "Update Agent";
+  editAgentId = agentId;
+  setCurrentView("Form");
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const firstName = document.getElementById("firstName").value;
+  const middleName = document.getElementById("middleName").value;
+  const lastName = document.getElementById("lastName").value;
+  const dob = document.getElementById("dob").value;
+  const heightInInches = document.getElementById("heightInInches").value;
+
+  const agent = {
+    firstName,
+    middleName,
+    lastName,
+    dob,
+    heightInInches: heightInInches ? parseInt(heightInInches) : 0
+  };
+
+  if (editAgentId > 0) {
+    doPut(agent);
+  } else {
+    doPost(agent);
+  }
+}
+
+// Helper Methods 
+function renderErrors(errors) {
+  const errorsHTML = errors.map((error) => `<li>${error}</li>`);
+  const errorsHTMLString = `<p>The following errors were found:</p>
+    <ul>
+    ${errorsHTML.join("")}
+    </ul>`;
+  document.getElementById("error").innerHTML = errorsHTMLString;
+}
+
+function resetState() {
+  document.getElementById("form").reset();
+  document.getElementById("formSubmitButton").innerText = "Add Agent";
+  document.getElementById("formHeading").innerText = "Add Agent";
+  document.getElementById("error").innerHTML = "";
+  editAgentId = 0;
+  setCurrentView("List");
+}
+
+function setCurrentView(view) {
+  const formContainerElement = document.getElementById("formContainer");
+  const listContainerElement = document.getElementById("listContainer");
+
+  switch (view) {
+    case "List":
+      formContainerElement.style.display = "none";
+      listContainerElement.style.display = "block";
+      break;
+    case "Form":
+      formContainerElement.style.display = "block";
+      listContainerElement.style.display = "none";
+      break;
+  }
+}
+
+displayList();
