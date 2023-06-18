05f2ac876cc6fafc6c08b2faa27c788f6713ae71
Chen <xchendd214@gmail.com>
2023-06-18 16:21:35 -0400

    styled the add agent, edit and delete buttons, and aligned the table output fields to correctly display with the headers

--- assessment/client/field-agent-react/src/components/Agents.jsx
+++ assessment/client/field-agent-react/src/components/Agents.jsx
@@ -58,46 +58,47 @@ const Agents = () => {
     <div>
       <main className="container font-007 text-2xl">
         <section id="listContainer">
-          <h2>Agents</h2>
+          <h2 className="px-4 text-4xl font-black">Agents</h2>
           <div className="border-solid">
-            <button onClick={() => navigate("/agents/add")}>
-              <i></i> Add Agent
+            <button
+              className="ml-2 px-2 py-1 text-white bg-black hover:bg-gray-800 rounded-sm"
+              onClick={() => navigate("/agents/add")}
+            >
+              Add Agent
             </button>
           </div>
           <table>
             <thead>
-              <tr>
-                <div className="flex py-6 px-4">
-                  <th className="px-4">First Name</th>
-                  <th className="px-4">Middle Name</th>
-                  <th className="px-4">Last Name</th>
-                  <th className="px-4">DOB</th>
-                  <th className="px-4">Height In Inches</th>
-                  <th>&nbsp;</th>
-                </div>
+              <tr className="flex py-6 px-4">
+                <th className="px-4">First Name</th>
+                <th className="px-4">Middle Name</th>
+                <th className="px-4">Last Name</th>
+                <th className="px-4">DOB</th>
+                <th className="px-4">Height In Inches</th>
+                <th>&nbsp;</th>
               </tr>
             </thead>
-            <tbody>
+            <tbody className="absolute px-4">
               {agents.map((agent) => (
                 <tr key={agent.id}>
-                  <td>{agent.firstName}</td>
-                  <td>{agent.middleName}</td>
-                  <td>{agent.lastName}</td>
-                  <td>{agent.dob}</td>
-                  <td>{agent.heightInInches}</td>
+                  <td className="px-4">{agent.firstName}</td>
+                  <td className="px-24">{agent.middleName}</td>
+                  <td className="px-4">{agent.lastName}</td>
+                  <td className="pr-4">{agent.dob}</td>
+                  <td className="px-16">{agent.heightInInches}</td>
                   <td>
-                    <div className="float-right mr-2">
+                    <div className="flex justify-end">
                       <Link
-                        className="border-solid"
+                        className="px-4 py-2 text-white bg-black hover:bg-gray-800 rounded mr-2 flex-1 text-center"
                         to={`/agents/edit/${agent.id}`}
                       >
-                        <i className="bi bi-pencil-square"></i> Edit
+                        Edit
                       </Link>
                       <button
-                        className="btn btn-danger btn-sm"
+                        className="px-4 py-2 text-black bg-white hover:bg-gray-200 rounded flex-1 text-center"
                         onClick={() => handleDeleteAgent(agent.id)}
                       >
-                        <i className="bi bi-trash"></i> Delete
+                        Delete
                       </button>
                     </div>
                   </td>
